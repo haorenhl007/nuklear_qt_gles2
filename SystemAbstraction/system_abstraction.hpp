65b01f6c76d2ac153136561a0e12cda31286e132
Rafal Kruszyna <rafal-tarnow@wp.pl>
2018-02-20 12:30:45 +0100

    czesciowo przeniesiono do SystemAbstraction

--- SystemAbstraction/system_abstraction.hpp
+++ SystemAbstraction/system_abstraction.hpp
@@ -1,5 +1,14 @@
 #pragma once
 
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#include "./SystemAbstraction/Application/libs/nuklear/nuklear.h"
+
+
 struct PointerCoords {
     float x, y;
 
